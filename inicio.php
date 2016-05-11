a28001a68fc0fd6f0e6e5d6c4ac58a3ffe8b482f
vortiz <vortiz@alumnos.uai.cl>
2016-05-11 13:50:08 -0300

    ho

---
+++ inicio.php
@@ -0,0 +1,12 @@
+<html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>FINANCIA TUS SUEÑOS E INVIERTE EN EL FUTURO</h1> 
+
+
+</body>
+</html>
--- newfile1.php
+++ newfile1.php
@@ -1,3 +1,16 @@
+<html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>FINANCIA TUS SUENOS E INVIERTE EN EL FUTURO</h1> 
+<h4> </h4>
+
+
+</body>
+</html>
 <?php
 // This file is part of Moodle - http://moodle.org/
 //
@@ -13,31 +26,39 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  *
  * @package local
- * @subpackage crowfounding
+ * @subpackage tics331
  * @copyright 2012-onwards Jorge Villalon <jorge.villalon@uai.cl>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 // Minimum for Moodle to work, the basic libraries
 require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
-$filter = required_param('filter', PARAM_TEXT);
+
 // Parameter passed from the url.
 $name = required_param('name', PARAM_TEXT);
+
 // Moodle pages require a context, that can be system, course or module (activity or resource)
 $context = context_system::instance();
 $PAGE->set_context($context);
+
 // Check that user is logued in the course.
 require_login();
+
 // Page navigation and URL settings.
-$PAGE->set_url(new moodle_url('/local/crowfounding', array('filter'=>$name)));
+$PAGE->set_url(new moodle_url('/local/tics331', array('filter'=>$name)));
 $PAGE->set_pagelayout('incourse');
-$PAGE->set_title('Hello world');
+//$PAGE->set_title('Hello world');
+
 // Show the page header
 echo $OUTPUT->header();
+
+
 // Here goes the content
-echo 'Hello world';
+//echo 'Hello world';
+
 // Show the page footer
 echo $OUTPUT->footer();
 ?>
